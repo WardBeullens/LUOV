9ffe63f6fbbd6c9850be7414b84a526c70f83358
Ward Beullens <ward@beullens.com>
2019-03-28 10:12:05 +0100

    Add Chacha-opt

--- AVX2_Optimized_Implementation/luov-8-58-237-chacha/PQCgenKAT_sign.c
+++ AVX2_Optimized_Implementation/luov-8-58-237-chacha/PQCgenKAT_sign.c
@@ -20,9 +20,6 @@
 #define KAT_DATA_ERROR      -3
 #define KAT_CRYPTO_FAILURE  -4
 
-
-void* aligned_alloc(int, int);
-
 int		FindMarker(FILE *infile, const char *marker);
 int		ReadHex(FILE *infile, unsigned char *A, int Length, char *str);
 void	fprintBstr(FILE *fp, char *S, unsigned char *A, unsigned long long L);
